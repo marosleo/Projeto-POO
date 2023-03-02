9ac1c0f58544a529585acf5d9a4b544682fa6fc5
marosleo <marosleo1990@gmail.com>
2023-03-02 16:27:45 -0300

    criando o corpo, estilizações e script básico

---
+++ javascript/js/script1.js
@@ -0,0 +1,18 @@
+class Tarefa {
+
+  constructor(){
+    this.id = 0;
+    this.nomeTarefa = '';
+    this.prioridade = 0;
+  }
+
+  adicionar() {
+    alert('Vamos adicionar um produto!')
+  }
+
+  excluir() {
+    alert('')
+  }
+}
+
+var tarefa = new Tarefa()
